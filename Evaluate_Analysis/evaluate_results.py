d4ffcc4888ca6c77370f8a6b831822ec92b4fdc1
Zhipeng Ding <ding.z.1@pg.com>
2018-11-02 23:24:50 -0400

    plot

--- Evaluate_Analysis/evaluate_results.py
+++ Evaluate_Analysis/evaluate_results.py
@@ -28,7 +28,7 @@ def Get_Dice_of_each_label(pred_label, gt_label):
 
     return dice_all
 
-def store_dice_score_data(pred_dir, gt_dir, img_num, label_num, save_name):
+def store_dice_score_data(pred_dir, gt_dir, img_num, label_num, save_name, res_type):
     """
     Save dice score of testing data for plot
     :param pred_dir: prediction label directory
@@ -39,10 +39,19 @@ def store_dice_score_data(pred_dir, gt_dir, img_num, label_num, save_name):
     :return: Dice score saved in a file
     """
 
-    res = np.zeros(img_num, label_num)
+    res = np.zeros((img_num, label_num))
 
     for i in range(33, 41):
-        pred_lab = pred_dir + 's' + str(i) + '_prediction_16_reflect.nii.gz'
+        if res_type == 'unet':
+            pred_lab = pred_dir + 's' + str(i) + '_prediction_16_reflect.nii.gz'
+        elif res_type == 'all_atlases':
+            pred_lab = pred_dir + 'all_atlases_majority_voting_s' + str(i) + '.nii.gz'
+        elif res_type == 'top_k_global':
+            pred_lab = pred_dir + 'top_6_atlases_majority_voting_s' + str(i) + '.nii.gz'
+        elif res_type == 'top_k_local':
+            pred_lab = pred_dir + 'top_6_atlases_local_majority_voting_s' + str(i) + '.nii.gz'
+        else:
+            raise TypeError('Undefined experiment type!')
         gt_lab = gt_dir + 's' + str(i) + '.nii'
 
         res[i-33, :] = Get_Dice_of_each_label(pred_lab, gt_lab)
@@ -51,43 +60,77 @@ def store_dice_score_data(pred_dir, gt_dir, img_num, label_num, save_name):
 
 
 if __name__ == '__main__':
-    res_unet_seg = np.zeros(8)
-    res_unet_seg_std = np.zeros(8)
-    res_all_majority_voting = np.zeros(8)
-    res_all_majority_voting_std = np.zeros(8)
-    res_top_6_majority_voting = np.zeros(8)
-    res_top_6_majority_voting_std = np.zeros(8)
+    ## save results as npy file
+    # store_dice_score_data(pred_dir='../U-Net/results/UNet3D_bias_LPBA40_LPBA40_fold_1_train_patch_72_72_72_batch_4_sampleThreshold_0.005_lr_0.001_scheduler_multiStep_10102018_005414/', gt_dir='../Data/LPBA40/corr_label/', img_num=8, label_num=57, save_name='./unet_seg_res.npy', res_type='unet')
+    # store_dice_score_data(pred_dir='../Label_Fusion/all_atlases_majority_voting/', gt_dir='../Data/LPBA40/corr_label/', img_num=8, label_num=57, save_name='./all_atlases_seg_res.npy', res_type='all_atlases')
+    # store_dice_score_data(pred_dir='../Label_Fusion/top_6_atlases_majority_voting/', gt_dir='../Data/LPBA40/corr_label/', img_num=8, label_num=57, save_name='./top_k_global_seg_res.npy', res_type='top_k_global')
+    # store_dice_score_data(pred_dir='../Label_Fusion/top_6_atlases_local_majority_voting/', gt_dir='../Data/LPBA40/corr_label/', img_num=8, label_num=57, save_name='./top_k_local_seg_res.npy', res_type='top_k_local')
 
-    for i in range(33, 41):
-        pred_lab = '../U-Net/results/UNet3D_bias_LPBA40_LPBA40_fold_1_train_patch_72_72_72_batch_4_sampleThreshold_0.005_lr_0.001_scheduler_multiStep_10102018_005414/s' + str(i) + '_prediction_16_reflect.nii.gz'
-        gt_lab = '../Data/LPBA40/corr_label/s' + str(i) + '.nii'
-        res_unet_seg[i-33] = np.mean(Get_Dice_of_each_label(pred_lab, gt_lab))
-        res_unet_seg_std[i - 33] = np.std(Get_Dice_of_each_label(pred_lab, gt_lab))
+    ## draw bar graph
+    res_unet_seg = np.load('./results_folder/unet_seg_res.npy')
+    res_unet_seg_mean = np.mean(res_unet_seg, axis=1)
+    res_unet_seg_std = np.std(res_unet_seg, axis=1)
 
-    for j in range(33, 41):
-        pred_lab = '../Label_Fusion/all_atlases_majority_voting/all_atlases_majority_voting_s' + str(j) + '.nii.gz'
-        gt_lab = '../Data/LPBA40/corr_label/s' + str(j) + '.nii'
-        res_all_majority_voting[j - 33] = np.mean(Get_Dice_of_each_label(pred_lab, gt_lab))
-        res_all_majority_voting_std[j - 33] = np.std(Get_Dice_of_each_label(pred_lab, gt_lab))
+    res_all_majority_voting = np.load('./results_folder/all_atlases_seg_res.npy')
+    res_all_majority_voting_mean = np.mean(res_all_majority_voting, axis=1)
+    res_all_majority_voting_std = np.std(res_all_majority_voting, axis=1)
 
-    for k in range(33, 41):
-        pred_lab = '../Label_Fusion/top_6_atlases_majority_voting/top_6_atlases_majority_voting_s' + str(j) + '.nii.gz'
-        gt_lab = '../Data/LPBA40/corr_label/s' + str(j) + '.nii'
-        res_top_6_majority_voting[k - 33] = np.mean(Get_Dice_of_each_label(pred_lab, gt_lab))
-        res_top_6_majority_voting_std[k - 33] = np.std(Get_Dice_of_each_label(pred_lab, gt_lab))
+    res_top_6_majority_voting = np.load('./results_folder/top_k_global_seg_res.npy')
+    res_top_6_majority_voting_mean = np.mean(res_top_6_majority_voting, axis=1)
+    res_top_6_majority_voting_std = np.std(res_top_6_majority_voting, axis=1)
+
+    res_top_6_local_majority_voting = np.load('./results_folder/top_k_local_seg_res.npy')
+    res_top_6_local_majority_voting_mean = np.mean(res_top_6_local_majority_voting, axis=1)
+    res_top_6_local_majority_voting_std = np.std(res_top_6_local_majority_voting, axis=1)
 
     ind = np.arange(len(res_unet_seg))  # the x locations for the groups
-    width = 0.3  # the width of the bars
+    width = 0.2  # the width of the bars
 
     fig, ax = plt.subplots()
-    rects1 = ax.bar(ind - width, res_unet_seg, width, yerr=res_unet_seg_std, color='SkyBlue', label='Unet')
-    rects2 = ax.bar(ind, res_all_majority_voting, width, yerr=res_all_majority_voting_std, color='IndianRed', label='All Majority Voting')
-    rects3 = ax.bar(ind + width, res_top_6_majority_voting, width, yerr=res_top_6_majority_voting_std, color='Blue', label='Top 6 Majority Voting')
+    rects1 = ax.bar(ind - 2*width, res_unet_seg_mean, width, yerr=res_unet_seg_std, color='SkyBlue', label='Unet')
+    rects2 = ax.bar(ind - width, res_all_majority_voting_mean, width, yerr=res_all_majority_voting_std, color='IndianRed', label='All Majority Voting')
+    rects3 = ax.bar(ind, res_top_6_majority_voting_mean, width, yerr=res_top_6_majority_voting_std, color='Blue', label='Top 6 Majority Voting')
+    rects4 = ax.bar(ind + width, res_top_6_local_majority_voting_mean, width, yerr=res_top_6_local_majority_voting_std, color='Red', label='Top 6 Local Majority Voting')
+
 
     ax.set_ylabel('Dice Score')
     ax.set_title('Scores by Methods')
     ax.set_xticks(ind)
     ax.set_xticklabels(('S33', 'S34', 'S35', 'S36', 'S37', 'S38', 'S39', 'S40'))
-    ax.legend()
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     fig.savefig('test.png', dpi=1000)
-    # plt.show()
+
+    #
+    # for i in range(33, 41):
+    #     pred_lab = '../U-Net/results/UNet3D_bias_LPBA40_LPBA40_fold_1_train_patch_72_72_72_batch_4_sampleThreshold_0.005_lr_0.001_scheduler_multiStep_10102018_005414/s' + str(i) + '_prediction_16_reflect.nii.gz'
+    #     gt_lab = '../Data/LPBA40/corr_label/s' + str(i) + '.nii'
+    #     res_unet_seg[i-33] = np.mean(Get_Dice_of_each_label(pred_lab, gt_lab))
+    #     res_unet_seg_std[i - 33] = np.std(Get_Dice_of_each_label(pred_lab, gt_lab))
+    #
+    # for j in range(33, 41):
+    #     pred_lab = '../Label_Fusion/all_atlases_majority_voting/all_atlases_majority_voting_s' + str(j) + '.nii.gz'
+    #     gt_lab = '../Data/LPBA40/corr_label/s' + str(j) + '.nii'
+    #     res_all_majority_voting[j - 33] = np.mean(Get_Dice_of_each_label(pred_lab, gt_lab))
+    #     res_all_majority_voting_std[j - 33] = np.std(Get_Dice_of_each_label(pred_lab, gt_lab))
+    #
+    # for k in range(33, 41):
+    #     pred_lab = '../Label_Fusion/top_6_atlases_majority_voting/top_6_atlases_majority_voting_s' + str(j) + '.nii.gz'
+    #     gt_lab = '../Data/LPBA40/corr_label/s' + str(j) + '.nii'
+    #     res_top_6_majority_voting[k - 33] = np.mean(Get_Dice_of_each_label(pred_lab, gt_lab))
+    #     res_top_6_majority_voting_std[k - 33] = np.std(Get_Dice_of_each_label(pred_lab, gt_lab))
+    #
+    # ind = np.arange(len(res_unet_seg))  # the x locations for the groups
+    # width = 0.3  # the width of the bars
+    #
+    # fig, ax = plt.subplots()
+    # rects1 = ax.bar(ind - width, res_unet_seg, width, yerr=res_unet_seg_std, color='SkyBlue', label='Unet')
+    # rects2 = ax.bar(ind, res_all_majority_voting, width, yerr=res_all_majority_voting_std, color='IndianRed', label='All Majority Voting')
+    # rects3 = ax.bar(ind + width, res_top_6_majority_voting, width, yerr=res_top_6_majority_voting_std, color='Blue', label='Top 6 Majority Voting')
+    #
+    # ax.set_ylabel('Dice Score')
+    # ax.set_title('Scores by Methods')
+    # ax.set_xticks(ind)
+    # ax.set_xticklabels(('S33', 'S34', 'S35', 'S36', 'S37', 'S38', 'S39', 'S40'))
+    # ax.legend()
+    # fig.savefig('test.png', dpi=1000)
+    # # plt.show()
